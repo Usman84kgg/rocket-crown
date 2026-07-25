5681d27a65db220aee809caa2e4953d0d884ff0e
Usman84kgg <usmanbinancep2p@gmail.com>
2026-07-26 02:06:45 +0300

    Update script.js

--- docs/wallet/script.js
+++ docs/wallet/script.js
@@ -1,1 +1,127 @@
-.
+// ОБЩИЙ БАЛАНС
+
+let totalBalanceUSD = 0;
+
+
+// АКТИВЫ ПОЛЬЗОВАТЕЛЯ
+
+const walletAssets = {
+
+USDT:0,
+TON:0,
+BTC:0,
+ETH:0,
+TRX:0,
+SOL:0,
+BNB:0,
+DOGE:0,
+XRP:0,
+LTC:0
+
+};
+
+
+// КУРСЫ КРИПТЫ
+
+const cryptoPrices = {
+
+USDT:1,
+TON:0,
+BTC:0,
+ETH:0,
+TRX:0,
+SOL:0,
+BNB:0,
+DOGE:0,
+XRP:0,
+LTC:0
+
+};
+
+
+// ОБНОВЛЕНИЕ БАЛАНСА
+
+function updateBalance(){
+
+
+totalBalanceUSD = 0;
+
+
+for(let coin in walletAssets){
+
+totalBalanceUSD +=
+walletAssets[coin] *
+cryptoPrices[coin];
+
+}
+
+
+console.log("TOTAL USD =",totalBalanceUSD);
+
+}
+
+
+// ПОПОЛНЕНИЕ
+
+function depositCrypto(){
+
+
+console.log("Deposit System Ready");
+
+
+}
+
+
+// ВЫВОД
+
+function withdrawCrypto(){
+
+
+console.log("Withdraw System Ready");
+
+
+}
+
+
+// ИСТОРИЯ
+
+function transactionHistory(){
+
+
+console.log("History System Ready");
+
+
+}
+
+
+// ОБНОВЛЕНИЕ КУРСОВ
+
+function updateCryptoPrices(){
+
+
+console.log("Crypto API Ready");
+
+
+}
+
+
+// TELEGRAM WALLET
+
+function connectTelegramWallet(){
+
+
+console.log("Telegram Wallet Ready");
+
+
+}
+
+
+// TON CONNECT
+
+function connectTONWallet(){
+
+
+console.log("TON Connect Ready");
+
+
+}
