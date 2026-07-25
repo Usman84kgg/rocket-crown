1a1473a4425fa688a0f59f8a6c27d70147484271
Usman84kgg <usmanbinancep2p@gmail.com>
2026-07-25 15:16:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-setTimeout(function(){
+setTimeout(function () {
 
-document.getElementById("loading-screen").style.display="none";
+document.getElementById("loading-screen").style.display = "none";
 
-document.getElementById("main-app").style.display="block";
+document.getElementById("main-app").style.display = "block";
 
-},2500);
+}, 2500);
+
+
+// Анимация баланса
+
+let balance = 0;
+
+setInterval(() => {
+
+balance += 0.01;
+
+document.querySelector(".balance-card h1").innerHTML =
+balance.toFixed(2) + " TON";
+
+}, 3000);
