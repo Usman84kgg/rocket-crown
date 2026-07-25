b08df255377918476ad80ef9b8ed4ee44b2eabea
Usman84kgg <usmanbinancep2p@gmail.com>
2026-07-25 20:01:20 +0300

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,21 @@
-.
+setTimeout(function () {
+
+document.getElementById("loading-screen").style.display = "none";
+
+document.getElementById("main-app").style.display = "block";
+
+}, 2500);
+
+
+// Анимация баланса
+
+let balance = 0;
+
+setInterval(() => {
+
+balance += 0.01;
+
+document.querySelector(".balance-card h1").innerHTML =
+balance.toFixed(2) + " TON";
+
+}, 3000);
